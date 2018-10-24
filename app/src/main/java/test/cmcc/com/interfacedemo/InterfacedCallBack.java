44a31d97f5c6b7305a17fe55b27c0bac5722123d
wsf <wangsifeng@qiyuexinxi.com>
2018-10-24 18:14:14 +0800

    Initial commit

---
+++ app/src/main/java/test/cmcc/com/interfacedemo/InterfacedCallBack.java
@@ -0,0 +1,9 @@
+package test.cmcc.com.interfacedemo;
+
+/**
+ * Created by wsf on 2018/10/23.
+ */
+
+public interface InterfacedCallBack {
+    void onMessage(String respose);
+}
---
+++ app/src/main/java/test/cmcc/com/interfacedemo/TestCallBack.java
@@ -0,0 +1,10 @@
+package test.cmcc.com.interfacedemo;
+
+/**
+ * Created by wsf on 2018/10/23.
+ */
+
+public interface TestCallBack {
+    void onFinish(String respose);
+    void onError(Exception e);
+}
